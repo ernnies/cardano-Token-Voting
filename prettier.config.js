41b0c7865191e570e2fdc5255cc08811e59226be
ernnies <ernestnnamdie@gmail.com>
2024-09-14 19:11:59 +0100

    postcss

--- prettier.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('prettier').Options} */
-module.exports = {
-  singleQuote: true,
-  semi: false,
-  trailingComma: 'all',
-  tabWidth: 2,
-  plugins: ['prettier-plugin-tailwindcss'],
-}
